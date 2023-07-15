897884d652530f8ca310fd46838b896b4a279574
VinayaAugusthy <vinayaaugusthy@gmail.com>
2023-07-15 21:30:53 +0530

    provider file done

---
+++ lib/list_provider.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class NumberListProvider extends ChangeNotifier {
+  List<int> numbers = [1, 2, 3, 4];
+  add() {
+    numbers.add(numbers.last++);
+  }
+}
